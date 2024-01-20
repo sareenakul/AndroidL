5f3315dfb265776272c373d6c2983cb43f05d908
Akul Sareen <sareen.akul@gmail.com>
2024-01-20 16:54:05 -0500

    Added the toggle button feature

--- app/src/main/java/com/example/myportfolio/MainActivity.kt
+++ app/src/main/java/com/example/myportfolio/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,6 +31,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -65,6 +69,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -93,14 +100,27 @@ fun CreateBizCard() {
                 imageProfile()
                 Divider(color = Color.LightGray, thickness = 1.dp)
                 profileDescription()
-                Button(onClick = { Log.d("Clicked", "CreateBizCard: ") }) {
+                Button(onClick = {
+                    //toggles it
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
                     Text("Portfolio",
                         style = MaterialTheme.typography.labelMedium)
+                }
+                if(buttonClickedState.value){
+                    Content()
+                }
+                else{
+                    Box(){
+
+                    }
                 }
             }
         }
     }
 }
+
+
 
 @Preview
 @Composable
@@ -109,13 +129,25 @@ fun Content(){
         .fillMaxWidth()
         .fillMaxHeight()
         .padding(5.dp)){
-        Surface(modifier = Modifier.padding(3.dp)
+        Surface(modifier = Modifier
+            .padding(3.dp)
             .fillMaxWidth()
             .fillMaxHeight(),
             shape = RoundedCornerShape(20.dp),
-            border = BorderStroke(2.dp, Color.Gray)
+            border = BorderStroke(2.dp, Color.Gray),
+            color = Color.LightGray,
         ) {
+            Portfolio(data = listOf("Project1", "Project2", "Project3"))
 
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data){ item ->
+            Text(item)
         }
     }
 }
